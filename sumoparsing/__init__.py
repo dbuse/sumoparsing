bdbc82b2aee45077b6d87dcb2830ff615be8a44c
Dominik Buse <dbuse@mail.uni-paderborn.de>
2019-02-27 11:24:27 +0100

    Bump version: 0.1.0 → 0.2.0

--- sumoparsing/__init__.py
+++ sumoparsing/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Dominik S. Buse"""
 __email__ = 'buse@ccs-lab.org'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .sumoparsing import iterparse_xml, get_result_field_types
 from .xmldescriptions import xml_type_configs
